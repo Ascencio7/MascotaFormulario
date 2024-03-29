a079f6be7e778da82680396e9ccd5dba33232530
Vladimir Ascencio <165211702+Ascencio7@users.noreply.github.com>
2024-03-29 13:29:33 -0600

    Complemento
    Complemento

---
+++ DAE/BaseMascotasContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Formulario_Mascotas.DAE;
+
+public partial class BaseMascotasContext : DbContext
+{
+    public BaseMascotasContext()
+    {
+    }
+
+    public BaseMascotasContext(DbContextOptions<BaseMascotasContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<DatosMascota> DatosMascotas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source= DESKTOP-E4D98NB\\SQLEXPRESS; Initial Catalog= BaseMascotas; Integrated Security= True; Encrypt= False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DatosMascota>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__DatosMas__3214EC27A784928B");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Contacto)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.FechaNacimiento).HasColumnType("datetime");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Raza)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DAE/DatosMascota.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Formulario_Mascotas.DAE;
+
+public partial class DatosMascota
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Apellido { get; set; }
+
+    public string? Raza { get; set; }
+
+    public DateTime? FechaNacimiento { get; set; }
+
+    public string? Contacto { get; set; }
+}
